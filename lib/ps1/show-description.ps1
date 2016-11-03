6cae94754e7207f235f84e8cf29e70e7bb627a3d
David Lejeune <Lejeune.David@outlook.com>
2016-11-03 22:07:37 +0100

    finished colors on base program

--- lib/ps1/show-description.ps1
+++ lib/ps1/show-description.ps1
@@ -1,18 +1,18 @@
 #feeding the narcistic beast
-"# Description"
-"# -----------"
-"# Windows Management tool "
-"# via homebrew powershell enviroment"
-"#"
-"# Author : David Lejeune"
-"# Created : 03/11/2016"
-"#"
-"# Task goals"
-"# ----------"
-"# Managing varying tasks on Windows OS via CLI"
-"# 1)Files handling"
-"#      - cleaning up files to File Hub"
-"#      - Logging and/or deleting big files"
-"# 2)Backup"
-"# 2)Handle GIT repositories"
-""
+write-host "# Description" -ForegroundColor DarkGray
+write-host "# -----------" -ForegroundColor DarkGray
+write-host "# Windows Management tool " -ForegroundColor DarkGray
+write-host "# via homebrew powershell enviroment" -ForegroundColor DarkGray
+write-host "#" -ForegroundColor DarkGray
+write-host "# Author : David Lejeune" -ForegroundColor DarkGray
+write-host "# Created : 03/11/2016" -ForegroundColor DarkGray
+write-host "#" -ForegroundColor DarkGray
+write-host "# Task goals" -ForegroundColor DarkGray
+write-host "# ----------" -ForegroundColor DarkGray
+write-host "# Managing varying tasks on Windows OS via CLI" -ForegroundColor DarkGray
+write-host "# 1)Files handling" -ForegroundColor DarkGray
+write-host "#      - cleaning up files to File Hub" -ForegroundColor DarkGray
+write-host "#      - Logging and/or deleting big files" -ForegroundColor DarkGray
+write-host "# 2)Backup" -ForegroundColor DarkGray
+write-host "# 2)Handle GIT repositories" -ForegroundColor DarkGray
+write-host "" -ForegroundColor DarkGray
