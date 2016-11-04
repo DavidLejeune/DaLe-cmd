0587b747d56ed47b2f8a511b25a083ebefa971f0
David Lejeune <Lejeune.David@outlook.com>
2016-11-04 23:08:03 +0100

    Timed measuring a folder

--- lib/ps1/get-directorysize.ps1
+++ lib/ps1/get-directorysize.ps1
@@ -1,4 +1,5 @@
-
+$StopWatch = [system.diagnostics.stopwatch]::startNew()
   Get-ChildItem $directory -Recurse | Measure-Object -Sum Length | Select-Object `
   @{Name="  # Files"; Expression={$_.Count}},
   @{Name="Size (Gb)"; Expression={$_.Sum /1024 /1024 / 1024 }} | format-list
+  write-host "Task completed in $($StopWatch.Elapsed | select TotalSeconds)`n" -ForegroundColor yellow
