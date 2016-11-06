f804c147168bab598a8bab3c52adb20b5a272ebd
David Lejeune <Lejeune.David@outlook.com>
2016-11-06 14:51:54 +0100

    automated the git work

---
+++ g.ps1
@@ -0,0 +1,37 @@
+#automation script for handling this git repo
+
+
+
+Write-Host '      ____              __        ' -ForegroundColor White
+Write-Host '     / __ \   ____ _   / /      ___ ' -ForegroundColor White
+Write-Host '    / / / /  / __ `/  / /      / _ \' -ForegroundColor White
+Write-Host '   / /_/ /  / /_/ /  / /___   /  __/' -ForegroundColor White
+Write-Host '  /_____/   \__,_/  /_____/   \___/ ' -ForegroundColor White
+Write-Host '' -ForegroundColor Yellow
+Write-Host '    +-+-+-+-+-+-+-+-+-+-+ +-+-+-+' -ForegroundColor Red
+Write-Host '    |P|o|w|e|r|s|h|e|l|l| |C|L|I|' -ForegroundColor Red
+Write-Host '    +-+-+-+-+-+-+-+-+-+-+ +-+-+-+' -ForegroundColor Red
+Write-Host '' -ForegroundColor Yellow
+Write-Host '  >> Author : David Lejeune' -ForegroundColor Gray
+Write-Host '  >> Created : 11/03/2016' -ForegroundColor Gray
+Write-Host ''
+write-host " #####################################" -ForegroundColor Magenta
+write-host "     Handling local GIT repository     " -ForegroundColor Red
+write-host " #####################################" -ForegroundColor Magenta
+write-host ""
+
+$Host.UI.RawUI.ForegroundColor = 'darkgreen'
+Write-Host "Adding all the (changed) files"
+git add .
+
+$Host.UI.RawUI.ForegroundColor = 'white'
+$message = Read-Host -Prompt 'Enter the commit message ';
+$message = [char]34 + $message + [char]34
+
+$Host.UI.RawUI.ForegroundColor = 'yellow'
+git commit -m $message
+
+
+$Host.UI.RawUI.ForegroundColor = 'magenta'
+Write-Host "Pushing that shit ....."
+git push lab master ; git push origin master
