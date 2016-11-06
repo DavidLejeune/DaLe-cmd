2f249ce2562a6704aea2ca22a29396c597f4eca2
David Lejeune <Lejeune.David@outlook.com>
2016-11-06 14:54:15 +0100

    small tweak to auto git

--- g.ps1
+++ g.ps1
@@ -13,7 +13,7 @@ Write-Host '    |P|o|w|e|r|s|h|e|l|l| |C|L|I|' -ForegroundColor Red
 Write-Host '    +-+-+-+-+-+-+-+-+-+-+ +-+-+-+' -ForegroundColor Red
 Write-Host '' -ForegroundColor Yellow
 Write-Host '  >> Author : David Lejeune' -ForegroundColor Gray
-Write-Host '  >> Created : 11/03/2016' -ForegroundColor Gray
+Write-Host '  >> Created : 06/11/2016' -ForegroundColor Gray
 Write-Host ''
 write-host " #####################################" -ForegroundColor Magenta
 write-host "     Handling local GIT repository     " -ForegroundColor Red
@@ -22,16 +22,19 @@ write-host ""
 
 $Host.UI.RawUI.ForegroundColor = 'darkgreen'
 Write-Host "Adding all the (changed) files"
+Write-Host ""
 git add .
 
 $Host.UI.RawUI.ForegroundColor = 'white'
 $message = Read-Host -Prompt 'Enter the commit message ';
 $message = [char]34 + $message + [char]34
-
 $Host.UI.RawUI.ForegroundColor = 'yellow'
 git commit -m $message
 
+Write-Host ""
 
 $Host.UI.RawUI.ForegroundColor = 'magenta'
 Write-Host "Pushing that shit ....."
 git push lab master ; git push origin master
+
+Write-Host ""
